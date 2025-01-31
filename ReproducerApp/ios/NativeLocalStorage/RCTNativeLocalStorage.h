168258895e69e8b46e224450e733ed35115f1c0a
Olivier Louvignes <olivier@mgcrea.io>
2025-01-31 15:06:15 +0100

    feat(modules): add sample obj-c turbo module

---
+++ ReproducerApp/ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,13 @@
+//  RCTNativeLocalStorage.h
+//  ReproducerApp
+
+#import <Foundation/Foundation.h>
+#import <ReproducerAppSpec/ReproducerAppSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
